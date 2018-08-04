e758b52bc86823448b2a0645eeb10cd6d2d352ab
BuildTools <unconfigured@null.spigotmc.org>
2018-08-05 00:18:52 +0200

    Just another commit

---
+++ src/main/java/com/projektg/wiuwiu1/gbot/Bot.java
@@ -0,0 +1,42 @@
+package com.projektg.wiuwiu1.gbot;
+
+import com.github.theholywaffle.teamspeak3.TS3Api;
+import com.github.theholywaffle.teamspeak3.TS3Config;
+import com.github.theholywaffle.teamspeak3.TS3Query;
+
+
+public class Bot {
+
+    private String ip;
+    private String nickname;
+    private String username;
+    private String pw;
+    private String vsId;
+
+    public Bot(String ip, String nickname, String username, String pw, String vsId){
+        this.ip = ip;
+        this.nickname = nickname;
+        this.username = username;
+        this.pw = pw;
+        this.vsId = vsId;
+    }
+
+    public void init(){
+        final TS3Config config = new TS3Config();
+        config.setHost(this.ip);
+
+        final TS3Query query = new TS3Query(config);
+        query.connect();
+
+        final TS3Api api = query.getApi();
+        api.login(username, pw);
+        api.selectVirtualServerById(1); //vsid anstatt 1
+        api.setNickname(this.nickname);
+        api.sendChannelMessage(this.nickname + " is online!");
+    }
+
+    @Override
+    public String toString() {
+        return "ip: " + ip + "nickname: " + nickname + "username: " + username + "pw: " + pw + "vsId: " + vsId;
+    }
+}
--- src/main/java/com/projektg/wiuwiu1/gbot/Init.java
+++ src/main/java/com/projektg/wiuwiu1/gbot/Init.java
@@ -1,7 +1,6 @@
 package com.projektg.wiuwiu1.gbot;
 
 import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
 
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -12,13 +11,15 @@ public class Init {
 
     public static void main(String[] args){
         try {
-            JsonReader reader = new JsonReader(new FileReader("conf.json"));
             Gson gson = new Gson();
-            Bot bot = gson.fromJson(reader, Bot.class);
+            System.out.println("creating bot..");
+            Bot bot = gson.fromJson(new FileReader("conf.json"), Bot.class);
+            System.out.println("starting bot (" + bot.toString() + ")...");
+            bot.init();
         } catch (IOException e){
             System.out.println("conf.json dont exists!");
         } finally {
-            System.out.println("building conf.json...");
+            System.out.println("building missing conf.json...");
             initJson();
         }
     }
@@ -28,10 +29,12 @@ public class Init {
             Writer writer = new FileWriter("conf.json");
             Gson gson = new Gson();
             gson.toJson("", writer);
+            // zurück zu try
         } catch (IOException e){
-            System.out.println("Error");
+            System.out.println("Error: coudnt build config json");
         } finally {
-
+            System.out.println("Shutdown");
+            System.exit(0);
         }
         return true;
     }
